ae958dba09770716bed9a099c407e888e76ed0c5
luiz-brancher <lppbrancher@senacrs.com.br>
2023-03-13 16:20:01 -0300

    pagina 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+checkBotoes();
+
+// caixas expansiveis
 $('.caixa-container button').click(function(){
     let caixa = $(this).prev()
     if(caixa.hasClass('aberta')){
@@ -8,3 +11,18 @@ $('.caixa-container button').click(function(){
         $(this).css('background-image', 'url("/assets/design/pg1/ico_saibamais_fecha.png")')
     }
 })
+
+// painel feminismo
+$('.painel-feminismo button').click(function(){
+    $(this).addClass('visitado')
+    console.log($(this).attr('data-bs-target'))
+    let visita = $(this).attr('data-bs-target')
+    localStorage.setItem(visita, 'visitado')
+})
+function checkBotoes(){
+    for(let i=1; i<=$('.painel-feminismo button').length; i++){
+        // console.log('o tamanho do array é '+$('.painel-feminismo button').length)
+        // console.log($('.modal-fem-'+i))
+        if(localStorage.getItem('#modal-fem-'+i)=='visitado'){$('.modal-fem-'+ i).addClass('visitado')}
+    }    
+}
